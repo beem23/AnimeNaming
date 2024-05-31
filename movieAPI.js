2519cafbe628ffd822e647be058409bcf1b0bfd5
beem23 <marlonbellot4@gmail.com>
2024-05-30 20:19:12 -0400

    First commit to movie API branch

---
+++ movieAPI.js
@@ -0,0 +1,4 @@
+//Here we will use the file names to look up the movie titles in the Movie API.
+//The movie API will return the movie title and the year it was released.
+//We will add ' (Year)' to the movie title.
+//We will then use the movie title to rename the file.
